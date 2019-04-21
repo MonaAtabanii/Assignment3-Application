d298c6cf684b66f6d8bc57bdae13bc6b067ac140
monaatabanii <monaatabani@gmail.com>
2019-04-21 11:12:18 -0700

    Add new project

---
+++ app/src/main/java/com/example/w3app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.w3app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "We are in the onCreate method of MainActivity");
+    }
+}
